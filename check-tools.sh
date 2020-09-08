1c9171d45bde21affd0e2477b5cb96701f6d4170
Tom Elam <tomelam@gmail.com>
2020-09-09 01:20:41 +0530

    Added more checks to check-tools.sh .

--- check-tools.sh
+++ check-tools.sh
@@ -8,7 +8,7 @@ if [[ -z "$(node --version)" ]] ; then
   exit 1
 else
   echo "On Windows & Mac, manually check that you are using the LTS version:"
-  echo "node -v"
+  echo "  node -v"
   echo "Get it at https://nodejs.org (https://nodejs.org/en/download/)."
   echo "As of September 8, 2020, the LTS version is v12.18.3 ."
   echo "Better use NVM (https://github.com/nvm-sh/nvm) to install and use it."
@@ -16,13 +16,35 @@ fi
 
 if ! npm list -g @dojo/cli > /dev/null ; then
   echo "@dojo/cli not installed globally. Please install it first:"
-  echo "npm i -g @dojo/cli"
-  exit 3
+  echo "  npm i -g @dojo/cli"
+  exit 2
 fi
 
 if ! npm list -g @dojo/cli-create-app > /dev/null ; then
   echo "@dojo/cli-create-app not installed globally. Please install it first:"
   echo "npm i -g @dojo/cli-create-app"
+  exit 4
+fi
+
+if ! npm list @dojo/cli-create-app > /dev/null ; then
+  echo "@dojo/cli-create-app not installed locally. Please install it first:"
+  echo "npm i @dojo/cli-create-app"
+  exit 4
+fi
+
+if ! npm list -g @dojo/cli-build-widget > /dev/null ; then
+  echo "@dojo/cli-build-widget not installed globally. Please install it first:"
+  echo "  npm i -g @dojo/cli-build-widget"
+  echo "This notification would have occurred anyway if you had straightaway"
+  echo "run 'dojo build widget'."
+  exit 3
+fi
+
+if ! npm list @dojo/cli-build-widget > /dev/null ; then
+  echo "@dojo/cli-build-widget not installed locally. Please install it first:"
+  echo "  npm i @dojo/cli-build-widget"
+  echo "This notification would have occurred anyway if you had straightaway"
+  echo "run 'dojo build widget'."
   exit 3
 fi
 
@@ -32,4 +54,10 @@ if ! npm list -g typescript > /dev/null ; then
   exit 3
 fi
 
-echo "The prerequisites are installed globally for building an app or widget."
+if ! npm list typescript > /dev/null ; then
+  echo "typescript not installed locally. Please install it first:"
+  echo "npm i typescript"
+  exit 3
+fi
+
+echo "The prerequisites are installed for building an app or widget."
